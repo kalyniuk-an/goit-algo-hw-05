9db56869a6d003a83fb66ec90d1040d9f52a79c5
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-07-20 08:55:17 +0300

    formatting

--- task_2.py
+++ task_2.py
@@ -1,15 +1,17 @@
 from typing import Callable
 
 def generator_numbers(text: str):
-  for s in text.split():
-    try:
-      yield float(s)
-    except ValueError:
-      continue
+    for s in text.split():
+        try:
+           yield float(s)
+        except ValueError:
+          continue
 
 def sum_profit(text: str, func: Callable):
-  return sum(func(text))
+    return sum(func(text))
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
 total_income = sum_profit(text, generator_numbers)
+
 print(f"Загальний дохід: {total_income}")
